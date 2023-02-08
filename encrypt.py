805c82cf3b60f944dd28471bf4b9cc5e02332ade
sterling <srkovash25@students.bmchs.org>
2023-02-08 08:44:41 -0600

    made it a loop

--- encrypt.py
+++ encrypt.py
@@ -1,9 +1,3 @@
-mode = input("would you like to encrypt or decrypt? (e/d) - ")
-
-key = int(input("which key would you like your cypher to use? (1-9) - "))
-
-message = input("enter a message to decrypt/encrypt - ").lower()
-
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 def encrypt():
@@ -21,8 +15,22 @@ def decrypt():
     for i in message:
         new = new + alphabet[alphabet.index(i)-key]
     print(new)
- 
-if mode == 'e':
-    encrypt()
-else:
-    decrypt()
+
+running = True
+while running:
+    mode = input("would you like to encrypt or decrypt? (e/d) - ")
+
+    key = int(input("which key would you like your cypher to use? (1-9) - "))
+
+    message = input("enter a message to decrypt/encrypt - ").lower()
+
+    if mode == 'e':
+        encrypt()
+    else:
+        decrypt()
+
+    cont = input("would you like to encrypt/decrypt again? (y/n) - ")
+    if cont == 'y':
+        pass
+    else:
+        running = False
